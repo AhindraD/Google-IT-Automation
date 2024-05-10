3c66de794081963f6a1ae24668f3250f8a88f0b8
Ahindra D <official.ahindra@gmail.com>
2024-05-10 22:03:49 +0530

    puppet_ntp manifest

---
+++ 05.ConfigMgmnt_Cloud/04.puppet_ntp.pp
@@ -0,0 +1,21 @@
+class ntp {
+  package { 'ntp':
+    ensure => latest,
+  } 
+  file { '/etc/ntp.conf':
+    source => '/home/user/ntp.conf',
+    replace => true,
+    require => Package['ntp'],
+    notify  => Service['ntp'],
+  }
+  service { 'ntp':
+    enable  => true,
+    ensure  => running,
+    require => File['/etc/ntp.conf'],
+  }
+}
+
+include ntp
+
+#command to apply manifest
+#sudo puppet apply -v ntp.pp
