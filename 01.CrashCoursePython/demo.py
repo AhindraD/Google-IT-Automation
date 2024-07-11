2e9de9a9c976b2810505dd4071456b37b0ec37bd
Ahindra D <official.ahindra@gmail.com>
2024-07-11 18:59:14 +0530

    certs

--- 01.CrashCoursePython/demo.py
+++ 01.CrashCoursePython/demo.py
@@ -1,3 +1,12 @@
 teacher_names = {"Math": "Aniyah Cook","Science": "Ines Bisset", "Engineering": "Wayne Branon"}
 
 print(teacher_names.values())
+
+feedbackApp-nextJs
+
+git config --global user.email "official.ahindra@gmail.com"
+git config --global user.name "AhindraD"
+
+
+sudo snap install ascii-image-converter
+ascii-image-converter -C path_to_image
